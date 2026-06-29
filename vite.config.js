98193c175ac5e4d95723d9b2d3f5112679ddbf66
Alexis Reyes <areyesmiralda@gmail.com>
2026-06-28 21:06:09 -0600

    cambio vite 2

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,45 @@
-import { defineConfig } from "vite";
-import path, { resolve } from "node:path";
-import * as glob from "glob";
+import { defineConfig } from "vite"
+import path, { resolve } from "node:path"
+import glob from "glob"
 
-import HtmlCssPurgePlugin from "vite-plugin-purgecss";
-import HandlebarPlugin from "vite-plugin-handlebars";
+import HtmlCssPurgePlugin from "vite-plugin-purgecss"
+import HandlebarPlugin from "vite-plugin-handlebars"
+
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 function obtenerHtmlFiles() {
-    return Object.fromEntries(
-        glob
-            .sync("./**/*.html", {
-                ignore: [
-                    "./dist/**",
-                    "./node_modules/**"
-                ]
-            })
-            .map((file) => [
-                file.slice(0, file.length - path.extname(file).length),
-                resolve(__dirname, file)
-            ])
-    );
+  return Object.fromEntries(
+    glob
+      .sync("**/*.html", {
+        ignore: ["dist/**", "node_modules/**"]
+      })
+      .map((file) => [
+        file.replace(".html", ""),
+        resolve(__dirname, file)
+      ])
+  )
 }
 
 export default defineConfig({
-    base: "/portafolio/",   // sin esto en git pages sale error 404 
+  base: "/portafolio/",
 
-    appType: "mpa",
+  appType: "mpa",
 
-    build: {
-        rollupOptions: {
-            input: obtenerHtmlFiles()
-        }
-    },
+  build: {
+    rollupOptions: {
+      input: obtenerHtmlFiles()
+    }
+  },
 
-    plugins: [
-        HandlebarPlugin({
-            partialDirectory: resolve(__dirname, "src/partials")
-        }),
+  plugins: [
+    HandlebarPlugin({
+      partialDirectory: resolve(__dirname, "src/partials")
+    }),
 
-        HtmlCssPurgePlugin()
-    ]
-});
+    HtmlCssPurgePlugin()
+  ]
+})
